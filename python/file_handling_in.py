49e05e01aa31e90071eee66305ba2c6e1cb9aa58
aganyo360 <wilsonaganyo@gmail.com>
2024-04-27 09:38:40 +0300

    renamed file.txt to pi_digits.txt

--- python/file_handling_in.py
+++ python/file_handling_in.py
@@ -1,1 +1,6 @@
 # You can read an entire file or read the file line by line
+# now let's open the file
+
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
