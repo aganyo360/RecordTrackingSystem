091ee02c321546cffb8a088464081078eadaf093
aganyo360 <wilsonaganyo@gmail.com>
2024-04-27 09:50:31 +0300

    reading a file in python

--- python/file_handling_in.py
+++ python/file_handling_in.py
@@ -1,6 +1,5 @@
 # You can read an entire file or read the file line by line
 # now let's open the file
-
-with open('pi_digits.txt') as file_object:
+with open("./pi.txt") as file_object:
     contents = file_object.read()
 print(contents)
