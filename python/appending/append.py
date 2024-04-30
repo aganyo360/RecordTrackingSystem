85a1bd6fc57bb62bfb26802eb6854dd28d6d92c5
aganyo360 <wilsonaganyo@gmail.com>
2024-04-30 11:42:05 +0300

    updated appending code

--- python/appending/append.py
+++ python/appending/append.py
@@ -1,6 +1,6 @@
 # add content to a file instead of writing over existing content, 
 # you can open the file in append mode.
 filename = './appendfile.txt'
-with open(filename) as file_object:
-    content = file_object.read()
-    print(content)
+with open(filename, 'a') as file_object:
+    file_object.write("\nthis is the apppend document")
+    
