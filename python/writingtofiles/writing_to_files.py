040fef5119cded5eb1cfc2c75030570d7d36639b
aganyo360 <wilsonaganyo@gmail.com>
2024-04-30 11:25:37 +0300

    updated filename

--- python/writingtofiles/writing_to_files.py
+++ python/writingtofiles/writing_to_files.py
@@ -1,3 +1,3 @@
-write_file = '../pi.txt'
-with open(write_file, "w") as file_object:
-    file_object.write( ' I am learnning reading and writing to files')
+filename = './write_file.txt'
+with open(filename, "w") as file_object:
+    file_object.write( ' I am learnning reading and writing to files\n this should create a new line\n it just work \n this is the fourth line')
