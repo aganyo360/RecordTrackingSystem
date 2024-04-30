d3f48ed39b19088a23a211d26046a74c11680cfd
aganyo360 <wilsonaganyo@gmail.com>
2024-04-30 20:09:04 +0300

    added first.py

--- python/appending/Exercise/first.py
+++ python/appending/Exercise/first.py
@@ -1,4 +1,4 @@
-
-with open('./guests.txt','a') as file_object:
-    name = input('Enter your name:')
+name = input("Enter your name:\n")
+with open("guests.txt",'a') as file_object:
     file_object.write(name)
+    print('Your name has been written to guests.txts')
