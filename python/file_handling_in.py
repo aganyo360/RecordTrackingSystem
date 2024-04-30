7c4e2262da933f32b7d7ab05e895dcfc3131f64c
aganyo360 <wilsonaganyo@gmail.com>
2024-04-30 11:29:09 +0300

    moved to new folder stucture

--- python/file_handling_in.py
+++
@@ -1,5 +0,0 @@
-# You can read an entire file or read the file line by line
-# now let's open the file
-with open("./pi.txt") as file_object:
-    contents = file_object.read()
-    print(contents.rstrip())
