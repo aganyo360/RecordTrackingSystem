cea6c8e61e01f3927169a5493624e6a56630734d
aganyo360 <wilsonaganyo@gmail.com>
2024-04-30 11:28:05 +0300

    commented explaantion of writint_to_files.py

--- python/writingtofiles/writing_to_files.py
+++ python/writingtofiles/writing_to_files.py
@@ -1,3 +1,8 @@
+
+# lets explain the code below
 filename = './write_file.txt'
 with open(filename, "w") as file_object:
     file_object.write( ' I am learnning reading and writing to files\n this should create a new line\n it just work \n this is the fourth line')
+
+#  at line 3 we create a variable to contain file path
+# open( filename, "w") the 'w' makes it to a read mode
